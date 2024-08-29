0107b9d21117f555ab6b8f32477d858d57af7880
Luis Barufi <luisbarufi@gmail.com>
2024-08-28 23:16:34 -0300

    Add package repositories

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,9 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	repository.Create(user)
 }
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
