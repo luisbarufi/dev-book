fe2f3238ad1c584b47d140419a62099ad831fa45
Luis Barufi <luisbarufi@gmail.com>
2024-08-28 23:32:09 -0300

    Adds user creation

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"api/src/models"
 	"api/src/repositories"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -27,7 +28,12 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	repository := repositories.NewUserRepository(db)
-	repository.Create(user)
+	userId, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", userId)))
 }
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
