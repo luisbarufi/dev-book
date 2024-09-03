d40c6d1b3684be1cb9d32e2083b0d7abbce704bb
Luis Barufi <luisbarufi@gmail.com>
2024-09-03 20:50:43 -0300

    Adds email validation

--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require github.com/gorilla/mux v1.8.1
 
 require (
+	github.com/badoux/checkmail v1.2.4
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/joho/godotenv v1.5.1
 )
--- api/src/models/User.go
+++ api/src/models/User.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 type User struct {
@@ -35,6 +37,10 @@ func (user *User) validate(step string) error {
 
 	if user.Email == "" {
 		return errors.New("o email é obrigatório e não pode estar em branco")
+	}
+
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		return errors.New("o email inserido é inválido")
 	}
 
 	if step == "register" && user.Password == "" {
