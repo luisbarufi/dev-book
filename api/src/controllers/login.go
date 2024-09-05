e0bfc24c6a6cc470ef36773e882c1081b03ce1cf
Luis Barufi <luisbarufi@gmail.com>
2024-09-04 22:58:01 -0300

    Adds login feature

---
+++ api/src/controllers/login.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/response_handler"
+	"api/src/security"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		response_handler.ErrorHandler(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		response_handler.ErrorHandler(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		response_handler.ErrorHandler(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	savedUser, err := repository.SearchByEmail(user.Email)
+	if err != nil {
+		response_handler.ErrorHandler(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err = security.VerifyPassword(savedUser.Password, user.Password); err != nil {
+		response_handler.ErrorHandler(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	w.Write([]byte("Você está logado! Parabéns!"))
+}
