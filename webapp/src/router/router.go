5e45a4c7fc52d935f415c67876edd4ddae988cf5
Luis Barufi <luisbarufi@gmail.com>
2024-09-20 20:22:54 -0300

    Fix the router package

--- webapp/src/router/router.go
+++ webapp/src/router/router.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"webapp/src/router/routes"
+
 	"github.com/gorilla/mux"
 )
 
 func Generate() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return routes.Configure(r)
 }
