af1248d19e7b15a5274e3952f3908937e968eb30
Luis Barufi <luisbarufi@gmail.com>
2024-10-01 23:19:29 -0300

    Change field name

--- webapp/src/models/Post.go
+++ webapp/src/models/Post.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Post struct {
-	ID          uint64    `json:"id,omitempty"`
-	Title       string    `json:"title,omitempty"`
-	Content     string    `json:"content,omitempty"`
-	Author_id   uint64    `json:"author_id,omitempty"`
-	Author_nick string    `json:"author_nick,omitempty"`
-	Likes       uint64    `json:"like"`
-	Created_at  time.Time `json:"created_at,omitempty"`
+	ID         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorId   uint64    `json:"author_id,omitempty"`
+	AuthorNick string    `json:"author_nick,omitempty"`
+	Likes      uint64    `json:"like"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
 }
