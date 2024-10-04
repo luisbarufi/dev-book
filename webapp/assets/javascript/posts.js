2eebaccf1d6165d5817ecc12ccabfdb53a3f0c16
Luis Barufi <luisbarufi@gmail.com>
2024-10-03 22:52:50 -0300

    Adds Post Like feature

--- webapp/assets/javascript/posts.js
+++ webapp/assets/javascript/posts.js
@@ -1,5 +1,5 @@
 $('#new-post').on('submit', createPost);
-
+$('.like-post').on('click', likePost);
 
 function createPost(event) {
   event.preventDefault();
@@ -15,5 +15,28 @@ function createPost(event) {
     window.location = "/home";
   }).fail(function(err) {
     alert("Erro ao criar a publicação!");
+  });
+}
+
+function likePost(event) {
+  event.preventDefault();
+
+  const elementClicked = $(event.target).closest('div').parent().closest('div')
+  const postId = elementClicked.data('post-id');
+
+  elementClicked.prop('disabled', true);
+
+  $.ajax({
+    url: `/posts/${postId}/like`,
+    method: `POST`,
+  }).done(function() {
+    const counterLikes = elementClicked.find('span');
+    const totalLikes = parseInt(counterLikes.text());
+
+    counterLikes.text(totalLikes + 1);
+  }).fail(function(err) {
+    alert("Erro ao curtir publicação!");
+  }).always(function() {
+    elementClicked.prop('disabled', false);
   });
 }
--- webapp/src/controllers/posts.go
+++ webapp/src/controllers/posts.go
@@ -5,9 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
 	"webapp/src/requests"
 	"webapp/src/responseHandler"
+
+	"github.com/gorilla/mux"
 )
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +27,30 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	url := fmt.Sprintf("%s/posts", config.ApiUrl)
 	response, err := requests.SendAuthenticatedRequest(r, http.MethodPost, url, bytes.NewBuffer(post))
+	if err != nil {
+		responseHandler.JSON(w, http.StatusInternalServerError, responseHandler.ApiErr{Err: err.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		responseHandler.HandleStatusCodeError(w, response)
+		return
+	}
+
+	responseHandler.JSON(w, response.StatusCode, nil)
+}
+
+func LikePost(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	postId, err := strconv.ParseInt(params["postId"], 10, 64)
+	if err != nil {
+		responseHandler.JSON(w, http.StatusBadRequest, responseHandler.ApiErr{Err: err.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/posts/%d/like", config.ApiUrl, postId)
+	response, err := requests.SendAuthenticatedRequest(r, http.MethodPost, url, nil)
 	if err != nil {
 		responseHandler.JSON(w, http.StatusInternalServerError, responseHandler.ApiErr{Err: err.Error()})
 		return
